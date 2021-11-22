d8c4ac1dc44fdc62959ce1c754bdd2e6e75bdbf5
JAYCHADAWAR <83951655+JAYCHADAWAR@users.noreply.github.com>
2021-11-22 20:34:00 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
         <>
 
             <div className="sidebar">
-                <h1 class='logo'>logo</h1>
+                <h1 class='logo'><span class="">l</span>ogo</h1>
                 <div class="navbar">
-                    <NavLink to="./" class="d"><GrOverview class="icon"/>Student Overview</NavLink>
-                    <NavLink to="./Stucoursepage" class="d" ><BsBookFill class="icon"/>Student Courses</NavLink>
-                    <NavLink to="./Stusettingpage" class="d"><FiSettings class="icon"/>Student Settings</NavLink>
-                    <NavLink to="./Stueprofile" class="d"><CgProfile class="icon"/>Student Profile</NavLink>
-                    <NavLink to="./Sturoadmap" class="d"><RiRoadMapFill class="icon"/>Student Roadmap</NavLink>
+                    <NavLink to="./" class="d"><GrOverview class="icon"/><span class="mitems">Student Overview</span></NavLink>
+                    <NavLink to="./Stucoursepage" class="d" ><BsBookFill class="icon"/><span class="mitems">Student Courses</span></NavLink>
+                    <NavLink to="./Stusettingpage" class="d"><FiSettings class="icon"/><span class="mitems">Student Settings</span></NavLink>
+                    <NavLink to="./Stueprofile" class="d"><CgProfile class="icon"/><span class="mitems">Student Profile</span></NavLink>
+                    <NavLink to="./Sturoadmap" class="d"><RiRoadMapFill class="icon"/><span class="mitems">Student Roadmap</span></NavLink>
                 </div>
 
             </div>
